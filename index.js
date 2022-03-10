45c066f7e0fb7d6fce1e5ff27c720bf39a3bb1ad
Shane McDonough <shanemcdman@gmail.com>
2022-03-10 12:36:33 -0800

    Add more options

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
+let els = {
+    'max_height': document.querySelector('#max-height'),
+    'min_height': document.querySelector('#min-height'),
+    'box_count': document.querySelector('#box-count'),
+    'delta_angle': document.querySelector('#delta-angle'),
+    'wavelength': document.querySelector('#wavelength'),
+};
 let boxCount = 5;
-let simWidth = 600;
+const SIM_WIDTH = 500;
 let angle = 0;
 const ROT_Y_ANGLE = Math.atan(1 / Math.sqrt(2));
 let maxD;
 
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
-    let orthoSize = simWidth * 3;
-    ortho(-simWidth, simWidth, -simWidth, simWidth, -1000, 1000);
-    maxD = dist(0,0, simWidth, simWidth);
+    let orthoSize = SIM_WIDTH * 3;
+    ortho(-SIM_WIDTH, SIM_WIDTH, -SIM_WIDTH, SIM_WIDTH, -1000, 1000);
+    maxD = dist(0,0, SIM_WIDTH, SIM_WIDTH);
 }
 
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight, WEBGL);
-    ortho(-simWidth, simWidth, -simWidth, simWidth, -1000, 1000);
+    ortho(-SIM_WIDTH, SIM_WIDTH, -SIM_WIDTH, SIM_WIDTH, -1000, 1000);
 }
 
 function draw(){
     background(100);
-    angle += 0.05;
+    angle += parseFloat(els.delta_angle.value);
     rotateX(-QUARTER_PI);
     rotateY(ROT_Y_ANGLE);
     drawBoxes();
 }
 
 function drawBoxes(){
-    let boxSize = simWidth / boxCount;
-    const SIM_CENTER = simWidth / 2 - boxSize / 2;
-    for(let x = 0; x < simWidth; x += boxSize){
-        for(let z = 0; z < simWidth; z += boxSize){
-            let a = dist(x, z, simWidth / 2 - boxSize / 2, simWidth / 2 - boxSize / 2);
-            a = map(a, 0, maxD, -1, 1)
+    let boxSize = SIM_WIDTH / els.box_count.value;
+    const SIM_CENTER = SIM_WIDTH / 2 - boxSize / 2;
+    let maxHeight = parseInt(els.max_height.value);
+    let minHeight = parseInt(els.min_height.value);
+    let wavelength = 200 - parseFloat(els.wavelength.value);
+    for(let x = 0; x < SIM_WIDTH; x += boxSize){
+        for(let z = 0; z < SIM_WIDTH; z += boxSize){
+            let a = dist(x, z, SIM_WIDTH / 2 - boxSize / 2, SIM_WIDTH / 2 - boxSize / 2);
+            a = map(a, 0, maxD, 0, wavelength)
             a += angle;
-            let height = floor(map(sin(a), -1, 1, 50, simWidth));
+            let height = floor(map(sin(a), -1, 1, minHeight, maxHeight));
             push();
             translate(x - SIM_CENTER, 0, z - SIM_CENTER);
             box(boxSize, height, boxSize);
